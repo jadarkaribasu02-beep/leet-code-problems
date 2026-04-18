4a89ee4ea49282d8b2ad208e4640b4965f816f05
karibasu jadar <jadarkaribasu02@gmail.com>
2026-04-18 20:38:29 +0530

    pr number 3783

---
+++ 3783.py
@@ -0,0 +1,12 @@
+#3783. Mirror Distance of an Integer
+class Solution:
+    def mirrorDistance(self, n: int) -> int:
+        karibasu = str(n)
+
+        reversed_karibasu = karibasu[::-1]  # Reverse the string
+
+        reversed_n = int(reversed_karibasu) #again it will convert to integer
+
+        return abs(n-reversed_n) # Return absolute difference
+
+       
